08231afe14d1948fb2ae23af9a063e97c0f96540
An4Laa <analaurarochadesouza6@gmail.com>
2023-10-11 11:11:49 -0300

    Atividade metragem

---
+++ metragem.py
@@ -0,0 +1,5 @@
+metragem = float(input("Digite a metragem: "))
+cm = metragem * 100
+mm = metragem * 1000
+print("A metragem em centímetros (cm) é: ", cm)
+print("A metragem em milímetros (mm) é: ", mm)
